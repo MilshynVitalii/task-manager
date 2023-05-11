50edd899d44505161ec67b9f04e15c5a38374c8e
Vitalii <vitalii.milshyn@computools.com>
2023-05-11 13:23:28 +0300

    Initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,49 @@
+module.exports = {
+  env: { es6: true, browser: true, node: true },
+  extends: [
+    "eslint:recommended",
+    "plugin:import/errors",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "module",
+    project: "tsconfig.json",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    "react-refresh",
+    "react",
+    "import",
+    "jsx-a11y",
+    "@typescript-eslint",
+  ],
+  rules: {
+    "react-refresh/only-export-components": "warn",
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+    "@typescript-eslint/no-empty-function": 0,
+    "@typescript-eslint/no-floating-promises": 0,
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+  return <h1>Initial commit</h1>;
+};
+
+export default App;
